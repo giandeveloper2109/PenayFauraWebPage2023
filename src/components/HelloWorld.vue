adc62ce59ceae83b9f43e478ec1a42222bb5f60a
Giancarlo Joel Alvarez Gallardo <gianalvarezgallardo21@gmail.com>
2023-12-11 11:51:47 -0500

    Primea Subida PyF

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,64 @@
+<template>
+
+  <v-carousel
+    cycle
+
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        :color="colors[i]"
+        height="100%"
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="text-h2">
+            {{ slide }} Slide
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        colors: [
+          'indigo',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+          'deep-purple accent-4',
+        ],
+        slides: [
+          'First',
+          'Second',
+          'Third',
+          'Fourth',
+          'Fifth',
+        ],
+      }
+    },
+  }
+</script>
+<style scoped> 
+.carousel-text {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5); /* Fondo semitransparente para el texto */
+  color: white;
+  padding: 10px;
+  text-align: center;
+}
+</style>
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,43 @@
+<!-- Navbar.vue -->
+
+<template>
+  <v-responsive>
+  <v-app-bar app class=" text-center d-flex flex-column" style="background:#950c15;">
+    <v-divider :thickness="20" class="border-opacity-0" vertical></v-divider>
+    
+    <div>
+      <v-img class="mr-3" src="@/assets/logoPyF.png" height="60px" width="60px"></v-img>
+    </div>
+    <div>
+      <v-toolbar-title style="color: aliceblue;" >Peña & Faura</v-toolbar-title>
+    </div>
+    <v-spacer></v-spacer>
+    <div>
+      <v-btn text style="color: aliceblue;">Quienes Somos</v-btn>
+    </div>
+    <v-divider :thickness="7" class="border-opacity-0" vertical></v-divider>
+    <div>
+      <v-btn text style="color: aliceblue;">Socios</v-btn>
+    </div>
+    <v-divider :thickness="7" class="border-opacity-0" vertical></v-divider>
+    <div>
+      <v-btn text style="color: aliceblue;">Servicios</v-btn>
+    </div>
+    <v-divider :thickness="7" class="border-opacity-0" vertical></v-divider>
+    <div>
+      <v-btn text style="color: aliceblue;">Contactenos</v-btn>
+    </div>
+    <v-divider :thickness="7" class="border-opacity-0" vertical></v-divider>
+    <div>
+      <v-img class="mr-3" src="@/assets/Idiomas/espana.png" height="20px" width="20px"></v-img>
+    </div>
+    <div>
+      <v-img class="mr-3" src="@/assets/Idiomas/ingles.png" height="20px" width="20px"></v-img>
+    </div>
+  </v-app-bar>
+</v-responsive>
+</template>
+
+<script setup>
+  //
+</script>
