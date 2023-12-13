a4a343da85f2efec9b407e21a0b62a6fabb30cba
Giancarlo Joel Alvarez Gallardo <gianalvarezgallardo21@gmail.com>
2023-12-13 12:32:34 -0500

    Avance menu Contactenos

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -24,8 +24,109 @@
       <v-btn text style="color: aliceblue;">Servicios</v-btn>
     </div>
     <v-divider :thickness="7" class="border-opacity-0" vertical></v-divider>
+    
     <div>
-      <v-btn text style="color: aliceblue;">Contactenos</v-btn>
+      <v-responsive>
+      <v-dialog
+        transition="dialog-top-transition"
+        width="800px"
+        
+      >
+        <template v-slot:activator="{ props }">
+          <v-btn
+          style="color: aliceblue;"
+            v-bind="props"
+          >Contactenos</v-btn>
+        </template>
+
+        <template v-slot:default="{ isActive }">
+
+          <v-card >
+            <!--
+            <v-toolbar style="background:#950c15;" class="v-toolbar-title"
+              title="Contactenos"
+            ></v-toolbar>
+            -->
+            
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-img class="mx-auto my-auto" width="300" height="300"  src="@/assets/logoPyF.png"></v-img>
+              </v-col>
+              
+              <v-divider :thickness="5" class="border-opacity-0" vertical></v-divider>
+              <v-col>
+          <v-card-text>
+              
+          <form @submit.prevent="submitForm">
+            <!-- Formulario Dialogo Contacenos PyF -->
+            
+            <v-row>
+              <v-col>
+                <v-text-field
+                  clearable
+                  label="Nombre"
+                  v-model="formData.name"
+                  prepend-icon="mdi-account"
+                  variant="solo-filled"
+                ></v-text-field>
+
+                <v-text-field
+                  clearable
+                  label="Telefono"
+                  v-model="formData.telefono"
+                  prepend-icon="mdi-cellphone"
+                  variant="solo-filled"
+                ></v-text-field>
+              </v-col>
+            
+            
+            
+              <v-col>
+                <v-text-field
+                  clearable
+                  label="Correo"
+                  v-model="formData.correo"
+                  prepend-icon="mdi-email"
+                  variant="solo-filled"
+                ></v-text-field>
+
+                <v-text-field
+                  clearable
+                  label="Asunto"
+                  v-model="formData.asunto"
+                  prepend-icon="mdi-card-account-mail"
+                  variant="solo-filled"
+                  ></v-text-field>
+                </v-col>
+            </v-row>
+            
+                <v-textarea
+                  clearable
+                  label="Label"
+                  prepend-icon="mdi-message"
+                  variant="solo-filled"
+                ></v-textarea>
+                
+            <!-- Otros campos del formulario -->
+
+            <v-btn type="submit">Enviar</v-btn>
+          </form>
+        </v-card-text>
+      </v-col>
+      </v-row>
+        <v-card-actions class="justify-end">
+              <v-btn
+                variant="text"
+                @click="isActive.value = false"
+              >Close</v-btn>
+            </v-card-actions>
+          
+          </v-card>
+        </template>
+      </v-dialog>  
+      </v-responsive>    
+
+      <!--<v-btn text style="color: aliceblue;">Contactenos</v-btn>-->
     </div>
     <v-divider :thickness="7" class="border-opacity-0" vertical></v-divider>
     <div>
@@ -39,5 +140,25 @@
 </template>
 
 <script setup>
-  //
+import { ref } from 'vue';
+
+const formData = ref({
+  name: '',
+  email: '',
+  // Agrega otros campos según tus necesidades
+});
+
+const submitForm = () => {
+  // Lógica para manejar el envío del formulario
+  console.log('Formulario enviado:', formData.value);
+  // Puedes agregar lógica adicional, como enviar los datos al servidor, etc.
+};
 </script>
+<style>
+  .v-toolbar-title{
+    color: rgb(255, 255, 255);
+    text-align: center;
+    font-size: 20px;
+    
+  }
+</style>
